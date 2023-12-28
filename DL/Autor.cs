d69cf76bd0713466e356d28f948e3a36018cc78b
ARodriguez1714 <jantoniochavezr14@gmail.com>
2023-12-28 09:57:57 -0600

    Agregar tablas/clases a la capa ML

---
+++ DL/Autor.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DL;
+
+public partial class Autor
+{
+    public byte IdAutor { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public string ApellidoPaterno { get; set; } = null!;
+
+    public string ApellidoMaterno { get; set; } = null!;
+
+    public byte[]? Foto { get; set; }
+
+    public byte? IdTipoAutor { get; set; }
+
+    public virtual TipoAutor? IdTipoAutorNavigation { get; set; }
+
+    public virtual ICollection<Medio> Medios { get; set; } = new List<Medio>();
+}
---
+++ DL/Direccion.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DL;
+
+public partial class Direccion
+{
+    public byte IdDireccion { get; set; }
+
+    public string Calle { get; set; } = null!;
+
+    public string NumeroInterior { get; set; } = null!;
+
+    public string NumeroExterior { get; set; } = null!;
+
+    public byte? IdColonia { get; set; }
+
+    public byte? IdEditorial { get; set; }
+
+    public virtual Colonium? IdColoniaNavigation { get; set; }
+
+    public virtual Editorial? IdEditorialNavigation { get; set; }
+}

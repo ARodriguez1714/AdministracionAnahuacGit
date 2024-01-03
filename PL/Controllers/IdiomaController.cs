8b16b5c52240819954ed436cac2a56b7997edcd8
JessGomez06 <jgilgomez6@gmail.com>
2024-01-03 16:30:51 -0600

    CRUD-Idioma sin Update

--- PL/Controllers/IdiomaController.cs
+++ PL/Controllers/IdiomaController.cs
@@ -19,7 +19,7 @@ namespace PL.Controllers
 
             if (result.Correct)
             {
-                idioma.Idiomas = result.Objects.ToList();
+                idioma.Idiomas = result.Objects;
                 return View(idioma);
             }
             else
@@ -31,95 +31,146 @@ namespace PL.Controllers
         }
 
         [HttpGet]
-        public ActionResult Form(int idIdioma)
+        public IActionResult GetAllIdioma()
         {
-            ML.Idioma idioma = new ML.Idioma();
-
-            if (idIdioma == 0) //null
+            ML.Result result = BL.Idioma.GetAllIdioma();
+            if (result.Correct)
             {
-               // ViewBag.Accion = "Agregar";
-                return View(idioma);
+                return Ok(result);
             }
             else
             {
-                ML.Result result = BL.Idioma.GetByIdIdioma(idIdioma);
-
-                if (result.Correct)
-                {
-                    idioma = (ML.Idioma)result.Object;
-                    return View(idioma);
-
-                }
-                ViewBag.Accion = "Actualizar";
-
+                return BadRequest(result);
             }
-
-            return View(idioma);
-
-
         }
 
-
         [HttpPost]
-        public ActionResult Form(ML.Idioma idioma)
+        public JsonResult Form(ML.Idioma idioma)
         {
             ML.Result result = new ML.Result();
-
             if (idioma.IdIdioma == null)
             {
-                //add 
                 result = BL.Idioma.AddIdioma(idioma);
 
                 if (result.Correct)
                 {
-                    ViewBag.Mensaje = result.Message;
-                    return View("Modal");
+                    return Json(result);
                 }
                 else
                 {
-                    ViewBag.Mensaje = result.Message;
-                    return View("Modal");
+                    return Json(result);
                 }
-
             }
             else
             {
                 result = BL.Idioma.UpdateIdioma(idioma);
-
                 if (result.Correct)
                 {
-                    ViewBag.Mensaje = result.Message;
-                    return View("Modal");
+                    return Json(result);
                 }
                 else
                 {
-                    ViewBag.Mensaje = result.Message;
-                    return View("Modal");
+                    return Json(result);
                 }
-
             }
-            //return View(idioma);
-
-
         }
 
 
-        [HttpGet]
-        public ActionResult Delete(int idIdioma)
+        //[HttpGet]
+        //public ActionResult Form(int idIdioma)
+        //{
+        //    ML.Idioma idioma = new ML.Idioma();
+
+        //    if (idIdioma == 0) //null
+        //    {
+        //       // ViewBag.Accion = "Agregar";
+        //        return View(idioma);
+        //    }
+        //    else
+        //    {
+        //        ML.Result result = BL.Idioma.GetByIdIdioma(idIdioma);
+
+        //        if (result.Correct)
+        //        {
+        //            idioma = (ML.Idioma)result.Object;
+        //            return View(idioma);
+
+        //        }
+        //        ViewBag.Accion = "Actualizar";
+
+        //    }
+
+        //    return View(idioma);
+
+
+        //}
+
+
+        //[HttpPost]
+        //public ActionResult Form(ML.Idioma idioma)
+        //{
+        //    ML.Result result = new ML.Result();
+
+        //    if (idioma.IdIdioma == null)
+        //    {
+        //        //add 
+        //        result = BL.Idioma.AddIdioma(idioma);
+
+        //        if (result.Correct)
+        //        {
+        //            ViewBag.Mensaje = result.Message;
+        //            return View("Modal");
+        //        }
+        //        else
+        //        {
+        //            ViewBag.Mensaje = result.Message;
+        //            return View("Modal");
+        //        }
+
+        //    }
+        //    else
+        //    {
+        //        result = BL.Idioma.UpdateIdioma(idioma);
+
+        //        if (result.Correct)
+        //        {
+        //            ViewBag.Mensaje = result.Message;
+        //            return View("Modal");
+        //        }
+        //        else
+        //        {
+        //            ViewBag.Mensaje = result.Message;
+        //            return View("Modal");
+        //        }
+
+        //    }
+        //    //return View(idioma);
+
+
+        //}
+
+        public JsonResult Delete(int idIdioma)
         {
-            //ML.Idioma idioma = new ML.Idioma();
             ML.Result result = BL.Idioma.DeleteIdioma(idIdioma);
-
-            if (result.Correct)
-            {
-                ViewBag.Mensaje = "Se ha eliminado correctamente";
-            }
-            else
-            {
-                ViewBag.Mensaje = "No se ha eliminado correctamente" + result.Message;
-            }
-            return PartialView("Modal");
-
+            return Json(result);
         }
+
+        //[HttpGet]
+        //public ActionResult Delete(int idIdioma)
+        //{
+        //    //ML.Idioma idioma = new ML.Idioma();
+        //    ML.Result result = BL.Idioma.DeleteIdioma(idIdioma);
+
+        //    if (result.Correct)
+        //    {
+        //        ViewBag.Mensaje = "Se ha eliminado correctamente";
+        //    }
+        //    else
+        //    {
+        //        ViewBag.Mensaje = "No se ha eliminado correctamente" + result.Message;
+        //    }
+        //    return PartialView("Modal");
+
+        //}
     }
 }
